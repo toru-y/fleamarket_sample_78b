9ba75d33db7b4fa9cfd885e6a5b30118a91824ee
toru-y <s6.7jjz@gmail.com>
2020-09-09 23:39:35 +0900

    added validates and association in user's model.
    # validates
    - presence
    - uniqueness
    - @ and domain(only email)
    - minimum numbers of characters
    
    # association
      has_many :purchases
      has_many :items
      has_many :comments
      has_one :credit_card

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :nickname, presence: true, uniqueness: true
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true  
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
+
+
+  has_many :purchases
+  has_many :items
+  has_many :comments
+  has_one :credit_card
 end
