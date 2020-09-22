b3e570b08029d0e8a7f16f2c7206c9bb1cca7533
toru-y <s6.7jjz@gmail.com>
2020-09-23 00:25:50 +0900

    Add association to item model
    The associated model is as below.
    - size
    - condition
    - shipping_fee
    - handling_time

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :size
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :handling_time
+  has_many :images
 end
