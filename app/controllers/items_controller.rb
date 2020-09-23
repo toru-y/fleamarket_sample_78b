27362866bdf0d4708826e55fcd9df7a1acf99874
toru-y <s6.7jjz@gmail.com>
2020-09-23 23:22:30 +0900

    Created a new registration method for item data.
    The added method is as follows.
    -new
    -create
    -set_item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @item = Item.new
+      @item.images.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @item = Item.new(set_item_params)
+    if @item.images.present?
+      @item.save
+    end
+    redirect_to root_path
+  end
+
+  private
+  def set_item_params
+    params.require(:item).permit(:name, :price, :description, :size, :category, :condition, :shipping_fee, :handling_time, :prefecture, :brand, images_attributes: [:image]).merge(user_id: current_user.id)
+  end
 end
