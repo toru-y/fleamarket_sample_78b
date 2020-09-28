8c0ca4c4fabcb82c6b74b554e28b852248d6990d
toru-y <s6.7jjz@gmail.com>
2020-09-29 02:04:01 +0900

    Added association and validation to the image model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :handling_time
-  has_many :images
+  belongs_to_active_hash :category
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates :name, :price, :description, :prefecture, :condition, :shipping_fee, :handling_time,  presence: true  
+  validates :name, :category_id, :size_id, :prefecture_id, :condition_id, :shipping_fee_id, :handling_time_id, :status, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
