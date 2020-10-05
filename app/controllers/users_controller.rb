d23dab4560cf64a8bf55813581af634b07f8bd16
toru-y <s6.7jjz@gmail.com>
2020-10-05 23:08:17 +0900

    generated user's controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user_params
+  before_action :move_to_root
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+  def set_user_params
+    @user = User.find(params[:id])
+  end
+
+  def move_to_root
+    unless user_signed_in? 
+      redirect_to root_path
+    end
+  end
 end
