a9d16f67929488ba56a2f227430bda6d868e998c
kazukiyokoo <kazuki102777@gmail.com>
2020-10-06 21:03:59 +0900

    create_category

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :handling_time
-  belongs_to_active_hash :category
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
---
+++ db/migrate/20201001024927_category.rb
@@ -0,0 +1,7 @@
+class Category < ActiveRecord::Migration[6.0]
+  def change
+    create_table :categories do |t|
+      t.string :name, null: false
+    end
+  end
+end
