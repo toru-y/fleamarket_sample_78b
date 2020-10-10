73e6333e78847a407fae0d5558b5f5383e5fade5
toru-y <s6.7jjz@gmail.com>
2020-10-10 11:58:35 +0900

    fix migrate error.
    - rename migration file.
    - add column (t.timestamps)

--- db/migrate/20201001024927_create_categories.rb
+++ db/migrate/20201001024927_create_categories.rb
@@ -1,7 +1,8 @@
-class Category < ActiveRecord::Migration[6.0]
+class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
       t.string :name, null: false
+      t.timestamps
     end
   end
 end
