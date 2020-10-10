5b538bab44f717b1b0d16cf843c5e7448b5071c9
toru-y <s6.7jjz@gmail.com>
2020-10-10 16:08:38 +0900

    added methods
    - before_action :move_to_user_registration
    - before_action :set_user_params
    - move_to_user_registration
    - set_user_prams

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,12 +1,13 @@
 class CreditCardsController < ApplicationController
   require 'payjp'
-  before_action :move_to_root
+  before_action :move_to_user_registration, only: [:show, :new, :create, :destroy]
+  before_action :set_user_params, only: [:show, :new]
 
   def show
     # 登録したカードを確認する、登録していない場合は空
     card = CreditCard.find_by(user_id: current_user.id)
     if card.present?
-      # flash[:notice] = 'クレジットカード情報の取得に成功しました。'
+      flash[:notice] = 'クレジットカード情報の取得に成功しました。'
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(card.customer_id)
       @card_info = customer.cards.retrieve(card.card_id)
@@ -22,10 +23,9 @@ class CreditCardsController < ApplicationController
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     # apiでトークンが取得できたか確認
     if params[:payjpToken].blank?
-      # flash.now[:alert] = "登録に失敗しました。内容を確認して再度登録してください。"
+      flash.now[:alert] = "登録に失敗しました。内容を確認して再度登録してください。"
       redirect_to action: :new
     else
-      # Payjpの継承オブジェクトCustomerに保存
       customer = Payjp::Customer.create(
         card: params[:payjpToken],
         metadata: {user_id: current_user.id}
@@ -34,10 +34,10 @@ class CreditCardsController < ApplicationController
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
 
       if @card.save
-        # flash.now[:notice] = 'クレジットカードの登録が完了しました。'
-        redirect_to card_path(id: @card.id)
+        flash.now[:notice] = 'クレジットカードの登録が完了しました。'
+        redirect_to credit_card_path(id: @card.id)
       else
-        # flash[:alert] = 'クレジットカード登録に失敗しました'
+        flash[:alert] = 'クレジットカード登録に失敗しました'
         redirect_to action: :new
       end
     end
@@ -51,16 +51,22 @@ class CreditCardsController < ApplicationController
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.destroy
-      # flash[:notice] = 'クレジットカード情報を削除しました'
+      flash[:notice] = 'クレジットカード情報を削除しました'
+      redirect_to action: :new
     else
-      # flash[:alert] = 'クレジットカード情報の削除に失敗しました'
+      flash[:alert] = 'クレジットカード情報の削除に失敗しました'
+      redirect_to action: :show
     end
-    redirect_to action: :new
+    
   end
 
   private
-  def move_to_root
-    # flash[:notice] = 'カードのご利用はユーザー登録をしてから行ってください。'
-    redirect_to ew_user_registration unless user_signed_in?
+
+  def move_to_user_registration
+    redirect_to new_user_registration unless user_signed_in?
+  end
+
+  def set_user_params
+    @user = User.find(current_user.id)
   end
 end
