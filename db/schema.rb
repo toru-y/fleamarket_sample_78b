487177851df75637d2f9a8acb544a3b344bdb12b
katsuo14 <67542683+katsuo14@users.noreply.github.com>
2020-10-11 18:37:27 +0900

    css_add3

--- db/schema.rb
+++ db/schema.rb
@@ -77,34 +77,6 @@ ActiveRecord::Schema.define(version: 2020_10_02_024331) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "labo_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.text "description"
-    t.string "image"
-    t.string "category"
-    t.string "brand"
-    t.string "size"
-    t.string "condition"
-    t.string "postage_player"
-    t.string "prefecture"
-    t.string "handling_times"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "labo_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_labo_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_labo_users_on_reset_password_token", unique: true
-  end
-
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id", null: false
     t.bigint "user_id", null: false
