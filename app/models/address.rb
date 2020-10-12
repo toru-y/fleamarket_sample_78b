065bdbf2b9f8caa8d0bfccff031ab9c13f9f0e22
toru-y <s6.7jjz@gmail.com>
2020-10-12 22:31:16 +0900

    added validates to parameters.

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,6 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :user
+
+  validates :post_code, :prefecture_id, :city, :house_number, presence: true
 end
