9d0cc2579f6bced405ea44e20ccf7998c3b51a2b
toru-y <s6.7jjz@gmail.com>
2020-10-14 00:05:45 +0900

    added routing to purchase.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
       get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
       get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
     end
+    
+    member do
+      get 'purchase'
+    end
   end
 
   resources :users, only: [] do
