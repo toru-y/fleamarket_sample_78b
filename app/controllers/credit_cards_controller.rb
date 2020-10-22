ab75fba0945c422bee79b2d500cc5aa1f0efbc3b
toru-y <s6.7jjz@gmail.com>
2020-10-22 22:39:09 +0900

    deleted unneccesary flash messages.

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -7,7 +7,6 @@ class CreditCardsController < ApplicationController
     # 登録したカードを確認する、登録していない場合は空
     card = CreditCard.find_by(user_id: current_user.id)
     if card.present?
-      flash[:notice] = 'クレジットカード情報の取得に成功しました。'
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(card.customer_id)
       @card_info = customer.cards.retrieve(card.card_id)
@@ -23,7 +22,6 @@ class CreditCardsController < ApplicationController
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     # apiでトークンが取得できたか確認
     if params[:payjpToken].blank?
-      flash.now[:alert] = "登録に失敗しました。内容を確認して再度登録してください。"
       redirect_to action: :new
     else
       customer = Payjp::Customer.create(
@@ -34,10 +32,8 @@ class CreditCardsController < ApplicationController
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
 
       if @card.save
-        flash.now[:notice] = 'クレジットカードの登録が完了しました。'
         redirect_to credit_card_path(id: @card.id)
       else
-        flash[:alert] = 'クレジットカード登録に失敗しました'
         redirect_to action: :new
       end
     end
@@ -51,10 +47,8 @@ class CreditCardsController < ApplicationController
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.destroy
-      flash[:notice] = 'クレジットカード情報を削除しました'
       redirect_to action: :new
     else
-      flash[:alert] = 'クレジットカード情報の削除に失敗しました'
       redirect_to action: :show
     end
     
